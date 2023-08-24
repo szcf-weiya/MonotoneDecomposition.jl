a6b1a0e6958899ac28112243dc3ffce038a31dc2
szcf-weiya <szcfweiya@gmail.com>
2023-08-24 19:09:18 -0400

    distinguish nrep

--- scripts/cmpr_monotest.jl
+++ scripts/cmpr_monotest.jl
@@ -12,18 +12,18 @@ function postfix()
 end
 
 function experiment_control_typeI_error(; resfolder = "/tmp", # "../res/mono_test/"
-                                          nrep = 100, kw...)
-    res = pmap(x->single_test_compare_mono(; kw...), 1:nrep)
-    serialize(joinpath(resfolder, "res_mono_test_mono_sup_nrep$(nrep)_$(postfix()).sil"), res)
+                                          NREP = 100, kw...)
+    res = pmap(x->single_test_compare_mono(; kw...), 1:NREP)
+    serialize(joinpath(resfolder, "res_mono_test_mono_sup_NREP$(NREP)_$(postfix()).sil"), res)
 end
 
-function experiment_bowman_example(; resfolder = "/tmp", nrep = 100, kw...)
-    res = pmap(x->single_test_compare_bowman(; kw...), 1:nrep)
-    serialize(joinpath(resfolder, "res_mono_test_bowman_sup_nrep$(nrep)_$(postfix()).sil"), res)
+function experiment_bowman_example(; resfolder = "/tmp", NREP = 100, kw...)
+    res = pmap(x->single_test_compare_bowman(; kw...), 1:NREP)
+    serialize(joinpath(resfolder, "res_mono_test_bowman_sup_NREP$(NREP)_$(postfix()).sil"), res)
 end
 
-function experiment_ghosal_example(; resfolder = "/tmp", nrep = 100, kw...)
-    res = pmap(x->single_test_compare_ghosal(; kw...), 1:nrep) 
-    serialize(joinpath(resfolder, "res_mono_test_ghosal_sup_nrep$(nrep)_$(postfix()).sil"), res)   
+function experiment_ghosal_example(; resfolder = "/tmp", NREP = 100, kw...)
+    res = pmap(x->single_test_compare_ghosal(; kw...), 1:NREP) 
+    serialize(joinpath(resfolder, "res_mono_test_ghosal_sup_NREP$(NREP)_$(postfix()).sil"), res)   
 end
 
